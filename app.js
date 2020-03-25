80d11a810f61fe0d26ae9726a25defbc423f1e23
Joana Martins <ajoanaverde@gmail.com>
2020-03-25 15:13:29 +0100

    v.jour1

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
+//
 // on demande à utiliser le module 'http'
+// ça va nous permetre de creer un server
+//
 const http = require('http');
+//
 // on appele le fichier ou on a construi les pages 
+//
 const routes = require('./routes');
-
-//on cree un server 
+//
+// on cree un server
+// en utilisant les routes qu'on a cree
+//
 const server = http.createServer(routes.handler);
-
-// informe le port à ecouter
+//
+// on informe le port à ecouter
+//
 server.listen(3000);
+//
+//
+//
+//
+//
+// http || https
+// http: unsecured, port 80, operates at application layer
+// https: secured, port 443, operates at transport layer
+// https requires SSL certificate that is signed by a CA (certificate authority)
+//
--- readme.md
+++ readme.md
@@ -1,15 +1,19 @@
 # Log de la quarantaine
 ![I'm sorry](https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fentries%2Ficons%2Ffacebook%2F000%2F028%2F021%2Fwork.jpg)
 
+#
+
+### Jour 3:
+- Revision du cours du 1° jour
+- Re-comenté le code
+- [ ] Exo de lundi (petit server avec 5 pages)
+
+#
+
 ### Jour 2:
 - Cours le matin
 - Exos js sur freecodecamp
 
-
-To do:
-- [ ] Revoir le cours du Jour 1
-
-#
 
 ### Jour 1 : 
 - Cours matin
